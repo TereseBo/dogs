c753e632560a148185f356b13bfd189aef3b8937
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-04-03 14:45:32 +0200

    Added error handeling for dogdetails and doglist

--- src/pages/Dogdetails.js
+++ src/pages/Dogdetails.js
@@ -2,29 +2,41 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 //Style
 import './Dogdetails.css'
+//Components
+import Msgbox from '../components/Msgbox'
 
 export default function Dogdetails() {
     const { dog } = useParams()
     const [images, setImages] = useState([])
+    const [msg, setMsg] = useState('Loading...');
+
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`https://dog.ceo/api/breed/${dog}/images/random/3`)
-            const data = await response.json()
-            setImages(data.message)
+            try {
+                const response = await fetch(`https://dog.ceo/api/breed/${dog}/images/random/3`)
+                if(response.status !== 200) throw new Error('Oops, something went wrong.')
+                const data = await response.json()
+                setImages(data.message)
+            } catch (error) {
+                setMsg('Oops, something went wrong.')
+            }
         }
         fetchData()
     }, [dog])
 
     return (
         <div className='Dogdetails'>
-            <h2>{dog}</h2>
-            <div className='image-container'>
-            {images.map((image, index) => {
-                return (
-                    <img key={`${dog}-img-${index}`} src={image} alt={`Dog beonging to the ${dog} race`} />
-                )
-            })}
-            </div>
+            {images.length>0 ? 
+            <>
+                <h2>{dog}</h2>
+                <div className='image-container'>
+                    {images.map((image, index) => {
+                        return (
+                            <img key={`${dog}-img-${index}`} src={image} alt={`Dog beonging to the ${dog} race`} />
+                        )
+                    })}
+                </div>
+            </> : <Msgbox msg={msg} />}
         </div>
 
     )
--- src/pages/Doglist.js
+++ src/pages/Doglist.js
@@ -1,20 +1,31 @@
 import {useState, useEffect} from 'react'
+//Style
+import './Doglist.css'
 //Components
 import List from '../components/List'
+import Msgbox from '../components/Msgbox';
 
 export default function Doglist(){
     const [dogs, setDogs] = useState([]);
+    const [msg, setMsg] = useState('Loading...');
     useEffect(() => {
         async function fetchData() {
+            try{
             const response = await fetch('https://dog.ceo/api/breeds/list/all')
+            if(response.status !== 200) throw new Error('Oops, something went wrong.')
             const data = await response.json()
             const array = Object.keys(data.message)
             setDogs(array);
+            } catch (error) {
+                setMsg('Oops, something went wrong.')
+            }
         }
         fetchData()
     }, [])
 
     return (
-        <List content={dogs}/>
+        <div className='Doglist'>
+        {dogs.length>0?<List content={dogs}/>: <Msgbox msg={msg}/>}
+        </div>
     )
 }
