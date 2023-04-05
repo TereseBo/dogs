b9e964767f884d2e22f50e297e3396062a1cb400
TereseBo <terese.boderus@gmail.com>
2023-04-05 13:49:32 +0200

    Tests added for Doglist

---
+++ src/AppDoglist.test.js
@@ -0,0 +1,63 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+import Doglist from './pages/Doglist';
+//import { act } from "react-dom/test-utils";
+
+test('Initially renders loading message', () => {
+  render(<App><Doglist /></App>);
+  const loadMsg = screen.getByText(/Loading/i);
+  expect(loadMsg).toBeInTheDocument();
+});
+
+test('Renders doglist on expected fetch-response', async () => {
+  const fakeDoglist = {
+    message: {
+      affenpinscher: [],
+      african: [],
+      airedale: [],
+      akita: [],
+      appenzeller: [],
+      australian: [
+        "shepherd"
+      ]
+    },
+    status: "success"
+  }
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve(fakeDoglist)
+    })
+  );
+  render(<App><Doglist /></App>);
+  const firstItem = await waitFor(() => screen.getByText(/affenpinscher/i))
+  const lastItem = await waitFor(() => screen.getByText(/australian/i))
+  expect(firstItem).toBeInTheDocument();
+  expect(lastItem).toBeInTheDocument();
+});
+
+test('Renders error message on unexpected fetch-response', async () => {
+  const fakeDoglist =
+  {
+    message: {
+      affenpinscher: [],
+      african: [],
+      airedale: [],
+      akita: [],
+      appenzeller: [],
+      australian: [
+        "shepherd"
+      ]
+    },
+    status: "success"
+  }
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      status: 400,
+      json: () => Promise.resolve(fakeDoglist)
+    })
+  );
+  render(<App><Doglist /></App>);
+  const errMsg = await waitFor(() => screen.getByText(/wrong/i))
+  expect(errMsg).toBeInTheDocument();
+});
